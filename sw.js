c378ebadd5cb6467525ba3d8958d6c1abc010944
arshisaleh <arshisaleh@gmail.com>
2018-09-27 06:07:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,8 +19,8 @@ let CACHE_FILES = [
     './img/8.jpg',
     './img/9.jpg',
     './img/10.jpg',
-    './https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-    './https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
+    '/https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+    '/https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
 ];
 
 

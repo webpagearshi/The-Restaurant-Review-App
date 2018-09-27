ab1f6575d80cb63135091bbdee4ed002e60cffce
arshisaleh <arshisaleh@gmail.com>
2018-09-27 12:59:08 +0530

    Update sw_register.js

--- js/sw_register.js
+++ js/sw_register.js
@@ -1,6 +1,6 @@
 	
 //registering service worker
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
         .then(function(registration) {
             console.log('Registration worked!,scope is:', registration.scope);
@@ -8,7 +8,7 @@ if ('serviceWorker' in navigator) {
         .catch(function(error) {
             console.log('Registration failed!', error);
         });
-}
+}*/
 /*if ('serviceWorker' in navigator) {
   window.addEventListener('load', function(){
     navigator.serviceWorker.register('sw.js').then(function(registration) {
@@ -21,3 +21,14 @@ if ('serviceWorker' in navigator) {
   } else {
 	console.log("no service worker in this browser");
 }*/
+if ('serviceWorker' in navigator) {
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    console.log('Service worker registration succeeded:', registration);
+  }, /*catch*/ function(error) {
+    console.log('Service worker registration failed:', error);
+  });
+} else {
+  console.log('Service workers are not supported.');
+}

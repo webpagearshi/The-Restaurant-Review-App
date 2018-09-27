d994ca46e7a664c51f0e6934254e0dc5cc72a76b
arshisaleh <arshisaleh@gmail.com>
2018-09-27 13:18:00 +0530

    Update sw_register.js

--- js/sw_register.js
+++ js/sw_register.js
@@ -12,7 +12,7 @@
 	console.log("no service worker in this browser");
 }*/
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('./sw.js')
+	navigator.serviceWorker.register('sw.js')
 	.then(function() {
 		console.log('Registration worked!');
 	})

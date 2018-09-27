3562b78bceee424a4efca6da55e19a6e07356564
arshisaleh <arshisaleh@gmail.com>
2018-09-27 10:26:09 +0530

    Update sw_register.js

--- js/sw_register.js
+++ js/sw_register.js
@@ -11,12 +11,13 @@
 }*/
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }).catch function(err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
-  });
-}    
+  } else {
+	console.log("no service worker in this browser");
+}
